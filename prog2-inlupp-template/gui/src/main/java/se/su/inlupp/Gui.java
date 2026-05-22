5ee8c51b2d718978c7fb1687c3993c94d8b35260
maximedalard14-coder <maximedalard14@gmail.com>
2026-05-22 14:15:30 +0200

    Update GUI layout and add file chooser
    Refactored GUI to use BorderPane layout and added file selection functionality.

--- prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,24 +1,39 @@
 package se.su.inlupp;
-
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+
 
 public class Gui extends Application {
 
   public void start(Stage stage) {
     Graph<String> graph = new ListGraph<String>();
+    BorderPane root = new BorderPane();
+    Pane center = new Pane();
+    root.setCenter(center);
+    Scene scene = new Scene(root , 640 , 480);
+
+
     String javaVersion = System.getProperty("java.version");
     String javafxVersion = System.getProperty("javafx.version");
-    Label label =
-        new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-
-    VBox root = new VBox(30, label);
-    root.setAlignment(Pos.CENTER);
-    Scene scene = new Scene(root, 640, 480);
+    Label label = new Label();
+    Button fileButton = new Button("Välj fil");
+    fileButton.setOnAction(e ->{
+      FileChooser fileChooser = new FileChooser();
+      fileChooser.setTitle("Välj en fil");
+      File file = fileChooser.showOpenDialog(stage);
+      if(file != null) {
+        label.setText("Vald fil: " + file.getName());
+      }
+    });
+    center.getChildren().addAll(fileButton , label);
     stage.setScene(scene);
     stage.show();
   }
