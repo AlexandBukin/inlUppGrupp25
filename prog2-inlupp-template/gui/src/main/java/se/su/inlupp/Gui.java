7f7eb78517122d0da2922a8f96df468c2985ebd2
Luka Nikolic <88612171+thelukanikolic@users.noreply.github.com>
2026-05-24 12:27:17 +0200

    Update Gui.java

--- prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
@@ -4,9 +4,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
@@ -18,57 +20,29 @@ public class Gui extends Application {
     Pane center = new Pane();
 
   public void start(Stage stage) {
-    Graph<String> graph = new ListGraph<String>();
     BorderPane root = new BorderPane();
     FlowPane top = new FlowPane();
     root.setTop(top);
     root.setCenter(center);
+    top.setBackground(Background.fill(Color.BLUE));
+    InteractionControl interactionControl = new InteractionControl(travelModel, center);
+
     Scene scene = new Scene(root , 640 , 480);
 
     Label label = new Label();
     Button fileButton = new Button("Välj fil");
-    fileButton.setOnAction(e ->{
-      FileChooser fileChooser = new FileChooser();
-      fileChooser.setTitle("Välj en fil");
-      File file = fileChooser.showOpenDialog(stage);
-      if(file != null) {
-        label.setText("Vald fil: " + file.getName());
-      }
-    });
-      Button addCityBtn = new Button("Lägg till stad");
-      addCityBtn.setOnAction(e -> {
-          center.setOnMouseClicked(mouseEvent -> {
-              double x = mouseEvent.getSceneX();
-              double y = mouseEvent.getSceneY();
-              TextInputDialog dialog = new TextInputDialog();
-              dialog.setTitle("Lägg till stad");
-              dialog.setHeaderText("Ange stadens namn");
-              dialog.showAndWait();
-              String name = dialog.getEditor().getText();
-              City city = new City(name, x, y);
-              travelModel.addCity(city);
-              addCityToGUI(travelModel, city, center);
-          });
-      });
-      top.getChildren().addAll(addCityBtn, fileButton);
+    fileButton.setOnAction(e -> interactionControl.loadFile(stage, label));
+
+    Button addCityBtn = new Button("Lägg till stad");
+    addCityBtn.setOnAction(e -> interactionControl.addCityClicked());
+      
+    top.getChildren().addAll(addCityBtn, fileButton);
     center.getChildren().addAll(label);
     stage.setScene(scene);
     stage.show();
   }
 
-    private void addCityToGUI(TravelModel travelModel, City city, Pane center) {
-
-      InteractionControl interactionControl = new InteractionControl();
-      interactionControl.interactableCity(travelModel, city, center);
-    }
-
-    public void redrawGraph() {
-        for (City city : travelModel.getCitys()) {
-            addCityToGUI(travelModel,city,center);
-        }
-    }
-
   public static void main(String[] args) {
     launch(args);
   }
-}
+} 
