6d068359b9be19b3f385a93af77c7f4803cc304c
AlexandBukin <alexandermelon.melon@gmail.com>
2026-05-25 00:13:29 +0200

    Update Gui.java
    La till spara funktion, Öppna funktion och lägg till kartbild

--- prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,10 +1,9 @@
 package se.su.inlupp;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
@@ -18,24 +17,53 @@ public class Gui extends Application {
 
     TravelModel travelModel = new TravelModel();
     Pane center = new Pane();
+    Stage stage;
 
     public void start(Stage stage) {
-        Graph<String> graph = new ListGraph<String>();
+        this.stage = stage;
+        Graph<String> graph = new ListGraph<String>(); //ta bort någon gång
         BorderPane root = new BorderPane();
         FlowPane top = new FlowPane();
         root.setTop(top);
         root.setCenter(center);
         Scene scene = new Scene(root , 640 , 480);
 
-        Label label = new Label();
-        Button fileButton = new Button("Välj fil");
-        fileButton.setOnAction(e ->{
+        MenuItem saveTravelApp = new MenuItem("Spara");
+        saveTravelApp.setOnAction(event -> {
+                String filePath = getFilePath();
+                travelModel.saveToFile(filePath);
+        });
+        MenuItem loadTravelApp = new MenuItem("Öppna");
+        loadTravelApp.setOnAction(event -> {
+            String filePath = getFilePath();
+                travelModel.loadFromFile(filePath);
+                redrawGraph();
+        });
+
+        MenuItem exitTravelApp = new MenuItem("Avsluta");
+        exitTravelApp.setOnAction(event -> {
+            if(!travelModel.hasUnsavedChanges()) {
+                stage.close();
+            }
+            else{
+                if(closeConfirmation()) {
+                    stage.close();
+                }
+            }
+        });
+
+        Menu menu = new Menu("Options...");
+        menu.getItems().addAll(saveTravelApp,loadTravelApp,exitTravelApp);
+        MenuBar menuBar = new MenuBar(menu);
+
+        Button addMapButton = new Button("Välj kartbild");
+        addMapButton.setOnAction(e ->{
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle("Välj en fil");
             File file = fileChooser.showOpenDialog(stage);
-            if(file != null) {
-                label.setText("Vald fil: " + file.getName());
-            }
+            String imagePath = file.getAbsolutePath();
+            travelModel.setImagePath(imagePath);
+            addMapToGUI(imagePath);
         });
 
         Button addCityBtn = new Button("Lägg till stad");
@@ -61,27 +89,55 @@ public class Gui extends Application {
                         center.setOnMouseClicked(null);
                     } else {
                         travelModel.addCity(city);
-                        addCityToGUI(travelModel, city, center);
+                        addCityToGUI(travelModel, city);
                     }
                 }
             });
         });
-        top.getChildren().addAll(addCityBtn, fileButton);
-        center.getChildren().addAll(label);
+
+        top.getChildren().addAll(addCityBtn, addMapButton, menuBar);
         stage.setScene(scene);
         stage.show();
     }
 
-    private void addCityToGUI(TravelModel travelModel, City city, Pane center) {
+    private void addMapToGUI(String imagePath) {
+        Image map = new Image(new File(imagePath).toURI().toString());
+        ImageView mapView = new ImageView(map);
+        mapView.fitHeightProperty().bind(stage.heightProperty());
+        mapView.fitWidthProperty().bind(stage.widthProperty());
+        center.getChildren().addFirst(mapView); //Kommer inte att skriva över förra bild om man vill ändra! Fixa!!!
+    }
 
+    private void addCityToGUI(TravelModel travelModel, City city) {
         InteractionControl interactionControl = new InteractionControl();
         interactionControl.interactableCity(travelModel, city, center);
     }
 
-    public void redrawGraph() {
+    private void redrawGraph() {
+        center.getChildren().clear();
         for (City city : travelModel.getCitys()) {
-            addCityToGUI(travelModel,city,center);
+            addCityToGUI(travelModel,city);
         }
+        addMapToGUI(travelModel.getImagePath());
+    }
+    private String getFilePath(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Välj en fil");
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            return file.getAbsolutePath();
+        }
+        return null;
+    }
+
+    private boolean closeConfirmation(){
+        Alert closeConfirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        closeConfirmationAlert.setTitle("Avsluta");
+        closeConfirmationAlert.setHeaderText("Är du säker på att du vill avsluta?");
+        closeConfirmationAlert.setContentText("Det finns osparade ändringar!");
+        Optional<ButtonType> result = closeConfirmationAlert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
     }
 
     public static void main(String[] args) {
