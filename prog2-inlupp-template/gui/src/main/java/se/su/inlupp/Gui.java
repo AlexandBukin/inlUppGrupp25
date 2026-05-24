effed976ef1ddb65424ab475ecf7881842bdea31
AlexandBukin <alexandermelon.melon@gmail.com>
2026-05-24 11:22:49 +0200

    Update Gui.java

--- prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Gui.java
@@ -1,17 +1,12 @@
 package se.su.inlupp;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextInputDialog;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Shape;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.io.File;
@@ -19,16 +14,14 @@ import java.io.File;
 
 public class Gui extends Application {
 
-    Shape circle;
-    double startX,startY;
+    TravelModel travelModel = new TravelModel();
+    Pane center = new Pane();
 
   public void start(Stage stage) {
-    TravelModel travelModel = new TravelModel();
     Graph<String> graph = new ListGraph<String>();
     BorderPane root = new BorderPane();
     FlowPane top = new FlowPane();
     root.setTop(top);
-    Pane center = new Pane();
     root.setCenter(center);
     Scene scene = new Scene(root , 640 , 480);
 
@@ -52,19 +45,9 @@ public class Gui extends Application {
               dialog.setHeaderText("Ange stadens namn");
               dialog.showAndWait();
               String name = dialog.getEditor().getText();
-              travelModel.addCity(new City(name, x, y));
-
-              Circle circle = new Circle(x, y, 10, Color.BLUE);
-
-              Label cityName = new Label("   "+name);
-              cityName.layoutXProperty().bind(circle.centerXProperty());
-              cityName.layoutYProperty().bind(circle.centerYProperty());
-
-              circle.setOnMousePressed(new StartDragHandler(circle));
-              circle.setOnMouseDragged(new DragHandler(circle));
-
-              center.getChildren().addAll(cityName,circle);
-              center.setOnMouseClicked(null);
+              City city = new City(name, x, y);
+              travelModel.addCity(city);
+              addCityToGUI(travelModel, city, center);
           });
       });
       top.getChildren().addAll(addCityBtn, fileButton);
@@ -73,37 +56,17 @@ public class Gui extends Application {
     stage.show();
   }
 
-  class StartDragHandler implements EventHandler<MouseEvent> {
+    private void addCityToGUI(TravelModel travelModel, City city, Pane center) {
 
-      private Circle circle;
+      InteractionControl interactionControl = new InteractionControl();
+      interactionControl.interactableCity(travelModel, city, center);
+    }
 
-      public StartDragHandler(Circle circle) {
-          this.circle = circle;
-      }
-
-      @Override
-      public void handle(MouseEvent event) {
-          startX = circle.getLayoutX();
-          startY = circle.getLayoutY();
-      }
-  }
-
-  class DragHandler implements EventHandler<MouseEvent> {
-
-      private Circle circle;
-
-      public DragHandler(Circle circle) {
-          this.circle = circle;
-      }
-
-      @Override
-      public void handle(MouseEvent event) {
-          double newX = circle.getLayoutX() + event.getX() - startX;
-          double newY = circle.getLayoutY() + event.getY() - startY;
-          circle.setCenterX(newX);
-          circle.setCenterY(newY);
-      }
-  }
+    public void redrawGraph() {
+        for (City city : travelModel.getCitys()) {
+            addCityToGUI(travelModel,city,center);
+        }
+    }
 
   public static void main(String[] args) {
     launch(args);
