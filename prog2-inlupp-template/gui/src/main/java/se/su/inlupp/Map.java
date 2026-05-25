2fe3f6da05f69b7bd798520a1157214cfffbff71
AlexandBukin <alexandermelon.melon@gmail.com>
2026-05-25 09:09:31 +0200

    Skapade en Map.java
    Är en Pane som är center för GUI (TravelApp), hanterar bilder just nu.

---
+++ prog2-inlupp-template/gui/src/main/java/se/su/inlupp/Map.java
@@ -0,0 +1,131 @@
+package se.su.inlupp;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
+import java.util.Optional;
+
+
+public class Gui extends Application {
+
+    Stage stage;
+    TravelModel travelModel = new TravelModel();
+    Map center = new Map(stage);
+
+    public void start(Stage stage) {
+        this.stage = stage;
+        Graph<String> graph = new ListGraph<String>(); //ta bort någon gång
+        BorderPane root = new BorderPane();
+        FlowPane top = new FlowPane();
+        root.setTop(top);
+        root.setCenter(center);
+        Scene scene = new Scene(root , 640 , 480);
+
+        MenuItem saveTravelApp = new MenuItem("Spara");
+        saveTravelApp.setOnAction(event -> {
+                String filePath = getFilePath();
+                travelModel.saveToFile(filePath);
+        });
+        MenuItem loadTravelApp = new MenuItem("Öppna");
+        loadTravelApp.setOnAction(event -> {
+            String filePath = getFilePath();
+                travelModel.loadFromFile(filePath);
+                redrawGraph();
+        });
+
+        MenuItem exitTravelApp = new MenuItem("Avsluta");
+        exitTravelApp.setOnAction(event -> {
+            if(!travelModel.hasUnsavedChanges()) {
+                stage.close();
+            }
+            else{
+                if(closeConfirmation()) {
+                    stage.close();
+                }
+            }
+        });
+
+        Menu menu = new Menu("Options...");
+        menu.getItems().addAll(saveTravelApp,loadTravelApp,exitTravelApp);
+        MenuBar menuBar = new MenuBar(menu);
+
+        Button addMapButton = new Button("Välj kartbild");
+        addMapButton.setOnAction(e ->{
+            center.promptImagePath(stage, travelModel);
+        });
+
+        Button addCityBtn = new Button("Lägg till stad");
+        addCityBtn.setOnAction(e -> {
+            center.setOnMouseClicked(mouseEvent -> {
+                double x = mouseEvent.getSceneX();
+                double y = mouseEvent.getSceneY();
+                TextInputDialog dialog = new TextInputDialog();
+                dialog.setTitle("Lägg till stad");
+                dialog.setHeaderText("Ange stadens namn");
+                Optional<String> result = dialog.showAndWait();
+                if(result.isEmpty()){
+                    center.setOnMouseClicked(null);
+                }
+                if(result.isPresent()) {
+                    String name = result.get();
+                    City city = new City(name, x, y);
+                    if (name.isEmpty() || travelModel.findCityByName(name) != null) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Fel");
+                        alert.setContentText("Stad namn inte giltigt eller redan finns");
+                        alert.showAndWait();
+                        center.setOnMouseClicked(null);
+                    } else {
+                        travelModel.addCity(city);
+                        addCityToGUI(travelModel, city);
+                    }
+                }
+            });
+        });
+
+        top.getChildren().addAll(addCityBtn, addMapButton, menuBar);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void addCityToGUI(TravelModel travelModel, City city) {
+        InteractionControl interactionControl = new InteractionControl();
+        interactionControl.interactableCity(travelModel, city, center);
+    }
+
+    private void redrawGraph() {
+        center.getChildren().clear();
+        center.hasImage = false;
+        for (City city : travelModel.getCitys()) {
+            addCityToGUI(travelModel,city);
+        }
+        center.addMapImage(stage, travelModel.getImagePath());
+    }
+    private String getFilePath(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Välj en fil");
+        File file = fileChooser.showOpenDialog(stage);
+
+        if (file != null) {
+            return file.getAbsolutePath();
+        }
+        return null;
+    }
+
+    private boolean closeConfirmation(){
+        Alert closeConfirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+        closeConfirmationAlert.setTitle("Avsluta");
+        closeConfirmationAlert.setHeaderText("Är du säker på att du vill avsluta?");
+        closeConfirmationAlert.setContentText("Det finns osparade ändringar!");
+        Optional<ButtonType> result = closeConfirmationAlert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
